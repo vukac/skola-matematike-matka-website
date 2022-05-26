c7a365ef9b9eac7207be3eb31eef08386b56ae59
Mateo Vuković <mateo.vukovic1998@protonmail.com>
2022-05-26 07:57:02 +0200

    Make the Home page responsive for larger devices
    Make the Home page responsive for desktops and larger tablets.

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,16 @@
 // Slider functionality
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  spaceBetween: 64,
+  // >=
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 80,
+    },
+    951: {
+      slidesPerView: 3,
+      spaceBetween: 64,
+    },
+  },
   navigation: {
     prevEl: '.testimonial-controllers__controller--prev',
     nextEl: '.testimonial-controllers__controller--next',
