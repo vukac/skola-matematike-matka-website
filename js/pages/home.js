c848c473634755ddfc4058c0759d9dbe9de3c5ba
Mateo Vuković <mateo.vukovic1998@protonmail.com>
2022-05-26 08:18:49 +0200

    Make the Home page responsive for smaller devices
    Make the Home page responsive for smartphones.

--- js/pages/home.js
+++ js/pages/home.js
@@ -3,9 +3,18 @@ const swiper = new Swiper('.swiper', {
   // >=
   breakpoints: {
     0: {
+      slidesPerView: 1,
+      /* If space between is not added, the next element will appear
+         next to current in the same view.
+       */
+      spaceBetween: 80,
+    },
+
+    601: {
       slidesPerView: 2,
       spaceBetween: 80,
     },
+
     951: {
       slidesPerView: 3,
       spaceBetween: 64,
